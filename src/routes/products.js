429b4dfb229e55f12ec4fd27c7d2f43cdeafdb44
German <gersilva96@hotmail.com>
2020-08-06 16:58:06 -0300

    Actualizo los endpoints de la API

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,6 +5,10 @@ const productsController = require("../controllers/productsController");
 
 router.get("/", productsController.root);
 
+router.get("/recents", productsController.recents);
+
+router.get("/commodities/total", productsController.comTotal);
+
 router.get("/:id", productsController.detail);
 
 module.exports = router;
