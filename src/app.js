5fa54224f7f10eaa18caaa8439bc84092cfcda44
German <gersilva96@hotmail.com>
2020-08-06 16:57:46 -0300

    Agrego cors para poder hacer peticiones desde el mismo servidor

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 require('dotenv').config();
 
 const mainRouter = require('./routes/main');
 const usersRouter = require('./routes/users');
 const productsRouter = require('./routes/products');
+const ordersRouter = require('./routes/orders');
 
 const app = express();
 
@@ -15,14 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors());
 
-app.use('/', mainRouter);
-app.use('/users', usersRouter);
-app.use('/products', productsRouter);
+app.use('/api/v1/', mainRouter);
+app.use('/api/v1/users', usersRouter);
+app.use('/api/v1/products', productsRouter);
+app.use('/api/v1/orders', ordersRouter);
 
 module.exports = app;
